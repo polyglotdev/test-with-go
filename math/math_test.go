c8b29fb8934a02881e59fb9eee365e466cc6ee0e
Dom Hallan <dom@polyglotdev.com>
2020-12-01 19:28:27 -0600

    test: add Sum test
    🧪 adding a test for a sum function in math package.

--- math/math_test.go
+++ math/math_test.go
@@ -1,11 +1,14 @@
 package math
 
-import "testing"
+import (
+	"testing"
 
-func TestMath(t *testing.T) {
-	want := 7
-	got := 7
-	if got != want {
-		t.Errorf("got: %q; want: %q", got, want)
+	"github.com/polyglotdev/test-with-go/instructor-code/math"
+)
+
+func TestSum(t *testing.T) {
+	sum := math.Sum([]int{10, -2, 3})
+	if sum != 11 {
+		t.Errorf("Wanted 11 but received %d", sum)
 	}
 }
